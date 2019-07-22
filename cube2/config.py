00e6279369f8950213bf01ac990b3707bf005d07
Tiberiu Boros <boros@adobe.com>
2019-07-22 18:19:19 +0300

    Tagger model update

--- cube2/config.py
+++ cube2/config.py
@@ -62,7 +62,7 @@ class TaggerConfig(Config):
     def __init__(self, filename=None, verbose=False):
         super().__init__()
         self.tagger_embeddings_size = 100
-        self.tagger_encoder_size = 100
+        self.tagger_encoder_size = 200
         self.tagger_encoder_layers = 2
         self.tagger_encoder_dropout = 0.5
         self.tagger_input_dropout_prob = 0.5
