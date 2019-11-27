97176974f5531680df5dd1b8e438c49e8cbc7ba3
Ubuntu <tiberiu44@gmail.com>
2019-11-27 08:15:40 +0000

    Updated default hyperparameters for parser

--- cube2/config.py
+++ cube2/config.py
@@ -98,7 +98,7 @@ class ParserConfig(Config):
     def __init__(self, filename=None, verbose=False):
         super().__init__()
         self.tagger_embeddings_size = 100
-        self.tagger_encoder_size = 300
+        self.tagger_encoder_size = 500
         self.tagger_encoder_layers = 5
         self.tagger_encoder_dropout = 0.33
         self.tagger_input_dropout_prob = 0.33
