78b1f1ef5fd2ee02abc03b09bcf5bf3814db7adc
Tiberiu Boros <tiberiu44@gmail.com>
2021-01-13 19:54:45 +0200

    Added XLM-Roberta support

--- cube3/networks/tagger.py
+++ cube3/networks/tagger.py
@@ -16,6 +16,8 @@ from cube3.io_utils.config import TaggerConfig
 import numpy as np
 from cube3.networks.modules import ConvNorm, LinearNorm
 import random
+from transformers import XLMRobertaTokenizer
+from transformers import XLMRobertaModel
 
 from cube3.networks.modules import WordGram
 
@@ -34,7 +36,7 @@ class Tagger(pl.LightningModule):
         self._id2lang = id2lang
 
         conv_layers = []
-        cs_inp = config.char_filter_size // 2 + config.lang_emb_size + config.word_emb_size
+        cs_inp = config.char_filter_size // 2 + config.lang_emb_size + config.word_emb_size + 768
         NUM_FILTERS = config.cnn_filter
         for _ in range(config.cnn_layers):
             conv_layer = nn.Sequential(
@@ -84,24 +86,32 @@ class Tagger(pl.LightningModule):
             self._early_stop_meta_val += 1
 
     def forward(self, x_sents, x_lang_sent, x_words_chars, x_words_case, x_lang_word, x_sent_len, x_word_len,
-                x_sent_masks, x_word_masks):
+                x_sent_masks, x_word_masks, x_word_emb_packed):
         char_emb_packed = self._word_net(x_words_chars, x_words_case, x_lang_word, x_word_masks, x_word_len)
 
-        blist = []
+        blist_char = []
+        blist_emb = []
         sl = x_sent_len.cpu().numpy()
         pos = 0
         for ii in range(x_sents.shape[0]):
-            slist = []
+            slist_char = []
+            slist_emb = []
             for jj in range(sl[ii]):
-                slist.append(char_emb_packed[pos, :].unsqueeze(0))
+                slist_char.append(char_emb_packed[pos, :].unsqueeze(0))
+                slist_emb.append(x_word_emb_packed[pos, :].unsqueeze(0))
                 pos += 1
             for jj in range(x_sents.shape[1] - sl[ii]):
-                slist.append(torch.zeros((1, self._config.char_filter_size // 2),
-                                         device=self._get_device(), dtype=torch.float))
-            sent_emb = torch.cat(slist, dim=0)
-            blist.append(sent_emb.unsqueeze(0))
+                slist_char.append(torch.zeros((1, self._config.char_filter_size // 2),
+                                              device=self._get_device(), dtype=torch.float))
+                slist_emb.append(torch.zeros((1, 768),
+                                             device=self._get_device(), dtype=torch.float))
+            sent_emb = torch.cat(slist_char, dim=0)
+            word_emb = torch.cat(slist_emb, dim=0)
+            blist_char.append(sent_emb.unsqueeze(0))
+            blist_emb.append(word_emb.unsqueeze(0))
 
-        char_emb = torch.cat(blist, dim=0)
+        char_emb = torch.cat(blist_char, dim=0)
+        word_emb_ext = torch.cat(blist_emb, dim=0)
         lang_emb = self._lang_emb(x_lang_sent)
         lang_emb = lang_emb.unsqueeze(1).repeat(1, char_emb.shape[1], 1)
 
@@ -114,27 +124,34 @@ class Tagger(pl.LightningModule):
         if self.training:
             mask_1 = np.ones((char_emb.shape[0], char_emb.shape[1]), dtype=np.long)
             mask_2 = np.ones((word_emb.shape[0], word_emb.shape[1]), dtype=np.long)
+            mask_3 = np.ones((word_emb.shape[0], word_emb.shape[1]), dtype=np.long)
 
             for ii in range(mask_1.shape[0]):
                 for jj in range(mask_1.shape[1]):
+                    mult = 1
                     p = random.random()
-                    if p < 0.3:
-                        pass
-                    elif p < 0.6:
-                        mask_1[ii, jj] = 2
-                        mask_2[ii, jj] = 0
-                    elif p < 0.8:
+                    if p < 0.33:
+                        mult += 1
                         mask_1[ii, jj] = 0
-                        mask_2[ii, jj] = 2
-                    else:
-                        mask_1[ii, jj] = 0
+                    p = random.random()
+                    if p < 0.33:
+                        mult += 1
                         mask_2[ii, jj] = 0
+                    p = random.random()
+                    if p < 0.33:
+                        mult += 1
+                        mask_3[ii, jj] = 0
+                    mask_1[ii, jj] *= mult
+                    mask_2[ii, jj] *= mult
+                    mask_3[ii, jj] *= mult
             mask_1 = torch.tensor(mask_1, device=self._get_device())
             mask_2 = torch.tensor(mask_2, device=self._get_device())
+            mask_3 = torch.tensor(mask_3, device=self._get_device())
             word_emb = word_emb * mask_1.unsqueeze(2)
             char_emb = char_emb * mask_2.unsqueeze(2)
+            word_emb_ext = word_emb_ext * mask_3.unsqueeze(2)
 
-        x = torch.cat([word_emb, lang_emb, char_emb], dim=-1)
+        x = torch.cat([word_emb, lang_emb, char_emb, word_emb_ext], dim=-1)
         x = x.permute(0, 2, 1)
         lang_emb = lang_emb.permute(0, 2, 1)
         half = self._config.cnn_filter // 2
@@ -167,10 +184,10 @@ class Tagger(pl.LightningModule):
         return optimizer
 
     def training_step(self, batch, batch_idx):
-        x_sent, x_lang, x_word_chars, x_word_case, x_lang_word, x_sent_len, x_word_len, x_sent_masks, x_word_masks, y_upos, y_xpos, y_attrs = batch
+        x_sent, x_lang, x_word_chars, x_word_case, x_lang_word, x_sent_len, x_word_len, x_sent_masks, x_word_masks, x_word_emb, y_upos, y_xpos, y_attrs = batch
         p_upos, p_xpos, p_attrs, a_upos, a_xpos, a_attrs = self.forward(x_sent, x_lang, x_word_chars, x_word_case,
                                                                         x_lang_word, x_sent_len, x_word_len,
-                                                                        x_sent_masks, x_word_masks)
+                                                                        x_sent_masks, x_word_masks, x_word_emb)
 
         loss_upos = F.cross_entropy(p_upos.view(-1, p_upos.shape[2]), y_upos.view(-1), ignore_index=0)
         loss_xpos = F.cross_entropy(p_xpos.view(-1, p_xpos.shape[2]), y_xpos.view(-1), ignore_index=0)
@@ -186,13 +203,14 @@ class Tagger(pl.LightningModule):
         return {'loss': step_loss}
 
     def validation_step(self, batch, batch_idx):
-        x_sent, x_lang, x_word_chars, x_word_case, x_lang_word, x_sent_len, x_word_len, x_sent_masks, x_word_masks, y_upos, y_xpos, y_attrs = batch
+        x_sent, x_lang, x_word_chars, x_word_case, x_lang_word, x_sent_len, x_word_len, x_sent_masks, x_word_masks, x_word_emb, y_upos, y_xpos, y_attrs = batch
         p_upos, p_xpos, p_attrs, a_upos, a_xpos, a_attrs = self.forward(x_sent, x_lang, x_word_chars, x_word_case,
                                                                         x_lang_word,
                                                                         x_sent_len,
                                                                         x_word_len,
                                                                         x_sent_masks,
-                                                                        x_word_masks)
+                                                                        x_word_masks,
+                                                                        x_word_emb)
 
         loss_upos = F.cross_entropy(p_upos.view(-1, p_upos.shape[2]), y_upos.view(-1), ignore_index=0)
         loss_xpos = F.cross_entropy(p_xpos.view(-1, p_xpos.shape[2]), y_xpos.view(-1), ignore_index=0)
@@ -291,9 +309,11 @@ class TaggerCollate:
     def collate_fn(self, batch: [Sentence]):
         a_sent_len = [len(sent.words) for sent in batch]
         a_word_len = []
+        x_word_embeddings = []
         for sent in batch:
             for word in sent.words:
                 a_word_len.append(len(word.word))
+                x_word_embeddings.append(word._emb)
         x_sent_len = np.array(a_sent_len, dtype=np.long)
         x_word_len = np.array(a_word_len, dtype=np.long)
         max_sent_len = np.max(x_sent_len)
@@ -348,6 +368,7 @@ class TaggerCollate:
                 c_word += 1
 
         x_lang_word = np.array(x_lang_word)
+
         return torch.tensor(x_sent), \
                torch.tensor(x_lang_sent), \
                torch.tensor(x_word), \
@@ -357,6 +378,7 @@ class TaggerCollate:
                torch.tensor(x_word_len), \
                torch.tensor(x_sent_masks), \
                torch.tensor(x_word_masks), \
+               torch.tensor(x_word_embeddings), \
                torch.tensor(y_upos), \
                torch.tensor(y_xpos), \
                torch.tensor(y_attrs)
@@ -386,23 +408,17 @@ class PrintAndSaveCallback(pl.callbacks.Callback):
             trainer.save_checkpoint(self.args.store + ".attrs")
         print("\n\nEpoch {}: {}, {}, {}\n".format(epoch, upos, xpos, attrs))
 
-        #from pprint import pprint
-        #pprint(metrics)
+        # from pprint import pprint
+        # pprint(metrics)
         for id in self._id2lang:
             lang = self._id2lang[id]
-            #upos = "UPOS = {:.4f}".format(metrics["val/UPOS/{0}".format(lang)])
             if pl_module._early_stop_results_lang_prev[lang]["upos"] < pl_module._early_stop_results_lang[lang]["upos"]:
-                #upos += " [BEST]"
                 trainer.save_checkpoint(self.args.store + "." + lang + ".upos")
-            #xpos = "XPOS = {:.4f}".format(metrics["val/XPOS/{0}".format(lang)])
             if pl_module._early_stop_results_lang_prev[lang]["xpos"] < pl_module._early_stop_results_lang[lang]["xpos"]:
-                #xpos += " [BEST]"
                 trainer.save_checkpoint(self.args.store + "." + lang + ".xpos")
-            #attrs = "ATTRS = {:.4f}".format(metrics["val/ATTRS/{0}".format(lang)])
-            if pl_module._early_stop_results_lang_prev[lang]["attrs"] < pl_module._early_stop_results_lang[lang]["attrs"]:
-                #attrs += " [BEST]"
+            if pl_module._early_stop_results_lang_prev[lang]["attrs"] < pl_module._early_stop_results_lang[lang][
+                "attrs"]:
                 trainer.save_checkpoint(self.args.store + "." + lang + ".attrs")
-            #print("\n{}: {}, {}, {}".format(lang, upos, xpos, attrs))
 
         trainer.save_checkpoint(self.args.store + ".last")
         lang_list = [self._id2lang[ii] for ii in self._id2lang]
@@ -416,6 +432,66 @@ class PrintAndSaveCallback(pl.callbacks.Callback):
             msg = "\t{0:30s}:\t{1:.4f}\t{2:.4f}\t{3:.4f}".format(lang, upos, xpos, attrs)
             print(msg)
         print("\n")
+
+
+class XLMHelper:
+    def __init__(self, device='cpu'):
+        self._splitter = XLMRobertaTokenizer.from_pretrained('xlm-roberta-base')
+        self._xlmr = XLMRobertaModel.from_pretrained("xlm-roberta-base", output_hidden_states=True)
+        self._xlmr.eval()
+        self._xlmr.to(device)
+        self._device = device
+
+    def _compute_we(self, batch: [Sentence]):
+        # XML-Roberta
+
+        # convert all words into wordpiece indices
+        word2pieces = {}
+        new_sents = []
+        START = 0
+        END = 1
+        PAD = 2
+        for ii in range(len(batch)):
+            c_sent = [START]
+            pos = 1
+            for jj in range(len(batch[ii].words)):
+                word = batch[ii].words[jj].word
+                pieces = self._splitter(word)['input_ids'][1:-1]
+                word2pieces[(ii, jj)] = []
+                for piece in pieces:
+                    c_sent.append(piece)
+                    word2pieces[(ii, jj)].append([ii, pos])
+                    pos += 1
+            c_sent.append(END)
+            new_sents.append(c_sent)
+        max_len = max([len(s) for s in new_sents])
+        input_ids = np.ones((len(new_sents), max_len), dtype=np.long) * 2  # pad everything
+        for ii in range(input_ids.shape[0]):
+            for jj in range(input_ids.shape[1]):
+                if jj < len(new_sents[ii]):
+                    input_ids[ii, jj] = new_sents[ii][jj]
+        with torch.no_grad():
+            we = self._xlmr(torch.tensor(input_ids, device=self._device))['last_hidden_state'].detach().cpu().numpy()
+
+        word_emb = []
+        for ii in range(len(batch)):
+            for jj in range(len(batch[ii].words)):
+                pieces = word2pieces[ii, jj]
+                m = we[pieces[0][0], pieces[0][1]]
+                for zz in range(len(pieces) - 1):
+                    m += we[pieces[zz][0], pieces[zz][1]]
+                m = m / len(pieces)
+                word_emb.append(m)
+        # word_emb = torch.cat(word_emb, dim=0)
+
+        return word_emb
+
+    def apply(self, doc: Document):
+        import tqdm
+        for sent in tqdm.tqdm(doc.sentences):
+            wemb = self._compute_we([sent])
+            for ii in range(len(wemb)):
+                sent.words[ii]._emb = wemb[ii]
 
 
 if __name__ == '__main__':
@@ -442,14 +518,16 @@ if __name__ == '__main__':
     # ensure target dir exists
     target = args.store
     i = args.store.rfind("/")
-    if i>0:
+    if i > 0:
         target = args.store[:i]
         os.makedirs(target, exist_ok=True)
 
     enc = Encodings()
     enc.compute(doc_train, None)
     enc.save('{0}.encodings'.format(args.store))
-
+    helper = XLMHelper(device='cuda:0')
+    helper.apply(doc_dev)
+    helper.apply(doc_train)
     trainset = TaggerDataset(doc_train)
     devset = TaggerDataset(doc_dev)
 
@@ -479,9 +557,9 @@ if __name__ == '__main__':
         accelerator=acc,
         num_nodes=1,
         default_root_dir='data/',
-        callbacks=[early_stopping_callback, PrintAndSaveCallback(args, id2lang)],
-        limit_train_batches = 5,
-        limit_val_batches = 2,
+        callbacks=[early_stopping_callback, PrintAndSaveCallback(args, id2lang)]
+        # limit_train_batches=5,
+        # limit_val_batches=2,
     )
 
     trainer.fit(model, train_loader, val_loader)
