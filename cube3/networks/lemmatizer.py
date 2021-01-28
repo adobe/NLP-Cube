f87f920b28a37b6f9dc2c3e88f827edc2403af9d
Tiberiu Boros <boros@adobe.com>
2021-01-28 11:02:37 +0200

    Lemmatizer in testing phase

--- cube3/networks/lemmatizer.py
+++ cube3/networks/lemmatizer.py
@@ -1,11 +1,9 @@
 import sys
-import optparse
 from typing import *
 
 import torch
-import tqdm
 import torch.nn as nn
-import torch.functional as F
+import torch.nn.functional as F
 import pytorch_lightning as pl
 import numpy as np
 
@@ -19,21 +17,21 @@ class Lemmatizer(pl.LightningModule):
     encodings: Encodings
     config: LemmatizerConfig
 
-    def __init__(self, config, encodings, num_languages=1, target_device='cpu'):
+    def __init__(self, config: LemmatizerConfig, encodings: Encodings, language_codes: [] = None):
         super(Lemmatizer, self).__init__()
         self._config = config
         self._encodings = encodings
-        self._num_languages = num_languages
-        self._target_device = target_device
+        self._num_languages = encodings.num_langs
+        self._language_codes = language_codes
 
         self._char_list = ['' for char in encodings.char2int]
         for char in encodings.char2int:
             self._char_list[encodings.char2int[char]] = char
-        self._lang_emb = nn.Embedding(num_languages + 1, config.lang_emb_size, padding_idx=0)
+        self._lang_emb = nn.Embedding(self._num_languages + 1, config.lang_emb_size, padding_idx=0)
         self._upos_emb = nn.Embedding(len(encodings.upos2int), config.upos_emb_size, padding_idx=0)
         self._char_emb = nn.Embedding(len(encodings.char2int) + 2, config.char_emb_size,
                                       padding_idx=0)  # start/stop index
-        self._case_emb = nn.Embedding(4, 16, padding_idx=0)  # 0-pad 1-upper 2-lower 3-symbol
+        self._case_emb = nn.Embedding(4, 16, padding_idx=0)  # 0-pad 1-symbol 2-upper 3-lower
         convolutions = []
         cs_inp = config.char_emb_size + config.lang_emb_size + config.upos_emb_size + 16
         for _ in range(3):
@@ -57,14 +55,20 @@ class Lemmatizer(pl.LightningModule):
                                 batch_first=True, bidirectional=False)
         self._attention = Attention(cs_inp // 2, config.decoder_size, config.att_proj_size)
 
-        self._output_char = LinearNorm(config.decoder_size, len(self._encodings.char2int))  # we use <PAD> for stop (0)
-        self._output_case = LinearNorm(config.decoder_size, 3)  # we use <PAD> for stop (0)
+        self._output_char = LinearNorm(config.decoder_size, len(self._encodings.char2int) + 2)
+        self._output_case = LinearNorm(config.decoder_size, 4)
         self._start_frame = nn.Embedding(1,
                                          config.encoder_size * 2 + config.char_emb_size + config.lang_emb_size + config.upos_emb_size + 16)
 
+        self._res = {}
+        for language_code in self._language_codes:
+            self._res[language_code] = {"loss": 0., "acc": 0.}
+        self._early_stop_meta_val = 0
+        self._epoch_results = None
+
     def forward(self, X):
         x_char = X['x_char']
-        x_case = X['x_char']
+        x_case = X['x_case']
         x_lang = X['x_lang']
         x_upos = X['x_upos']
         if 'y_char' in X:
@@ -77,7 +81,7 @@ class Lemmatizer(pl.LightningModule):
         lang_emb = self._lang_emb(x_lang).unsqueeze(1).repeat(1, char_emb.shape[1], 1)
         conditioning = torch.cat((case_emb, upos_emb, lang_emb), dim=-1)
         if gs_output is not None:
-            output_idx = self._get_output_idx(gs_output)
+            output_idx = gs_output
 
         x = torch.cat((char_emb, conditioning), dim=-1)
         x = x.permute(0, 2, 1)
@@ -125,8 +129,8 @@ class Lemmatizer(pl.LightningModule):
 
             output_char = self._output_char(decoder_output)
             output_case = self._output_case(decoder_output)
-            out_char_list.append(output_char.unsqueeze(1))
-            out_case_list.append(output_case.unsqueeze(1))
+            out_char_list.append(output_char)
+            out_case_list.append(output_case)
             selected_chars = torch.argmax(output_char, dim=-1)
             for ii in range(selected_chars.shape[0]):
                 if selected_chars[ii].squeeze() == 0:
@@ -150,11 +154,6 @@ class Lemmatizer(pl.LightningModule):
             return 'cpu'
         return '{0}:{1}'.format(self._char_emb.weight.device.type, str(self._char_emb.weight.device.index))
 
-    def _get_output_idx(self, x):
-        tmp = [[0, 0, 0, lemma] for lemma in x]
-        target_char, target_case = _get_targets(tmp, self._encodings)
-        return torch.tensor(target_char, dtype=torch.long, device=self._get_device())
-
     def process(self, sequences, lang_id):
         self.eval()
         pass
@@ -176,260 +175,86 @@ class Lemmatizer(pl.LightningModule):
         loss_case = F.cross_entropy(y_case_pred.view(-1, y_case_pred.shape[2]), y_case_target.view(-1), ignore_index=0)
         loss = loss_char + loss_case
         ok = 0
+
+        language_result = {lang_id: {'total': 0, 'ok': 0}
+                           for lang_id in range(self._num_languages)}
+
         y_char_target = y_char_target.detach().cpu().numpy()
-        y_char_pred = y_char_pred.detach().cpu().numpy()
-        for y_pred, y_target in zip(y_char_pred, y_char_target):
+        y_char_pred = torch.argmax(y_char_pred.detach(), dim=-1).cpu().numpy()
+        lang = batch['x_lang'].detach().cpu().numpy()
+        for lang_id, y_pred, y_target in zip(lang, y_char_pred, y_char_target):
             valid = True
             for y_p, y_t in zip(y_pred, y_target):
                 if y_t != 0 and y_p != y_t:
                     valid = False
                     break
             if valid:
-                ok += 1
-        total = len(batch)
-        return {'loss': loss, 'ok': ok, 'total': total}
+                language_result[lang_id - 1]['ok'] += 1
+            language_result[lang_id - 1]['total'] += 1
+
+        return {'loss': loss, 'acc': language_result}
 
     def validation_epoch_end(self, outputs: List[Any]) -> None:
-        pass
+        language_result = {lang_id: {'total': 0, 'ok': 0}
+                           for lang_id in
+                           range(self._num_languages)}
+        loss = 0
+        for result in outputs:
+            loss += result['loss'].item()
+            for lang_id in result['acc']:
+                language_result[lang_id]['ok'] += result['acc'][lang_id]['ok']
+                language_result[lang_id]['total'] += result['acc'][lang_id]['total']
+        loss = loss / len(outputs)
 
+        res = {}
+        for lang_index in language_result:
+            total = language_result[lang_index]['total']
+            if total == 0:
+                total = 1
+            if self._language_codes is None:
+                lang = lang_index
+            else:
+                lang = self._language_codes[lang_index]
+            res[lang] = {
+                "acc": language_result[lang_index]['ok'] / total,
+            }
 
-def _eval(lemmatizer, dataset):
-    ok = 0
-    total = 0
-    batches = _get_batches(dataset, 32)
-    lemmatizer.eval()
-    with torch.no_grad():
-        for batch in tqdm.tqdm(batches, ncols=80, desc='\tEvaluating'):
-            x_chars = []
-            x_lang = []
-            x_upos = []
-            for ii in range(len(batch)):
-                x_chars.append(batch[ii][0])
-                x_lang.append(batch[ii][1])
-                x_upos.append(batch[ii][2])
+            self.log('val/ACC/{0}'.format(lang), language_result[lang_index]['ok'] / total)
+        self.log('val/LOSS'.format(lang), loss)
 
-            y_char_target, y_case_target = _get_targets(batch, lemmatizer._encodings)
-            y_char_pred, y_case_pred = lemmatizer(x_chars, x_upos, x_lang)
-            good = np.ones(y_char_target.shape[0])
-            y_char_pred = torch.argmax(y_char_pred, dim=-1).cpu().numpy()
-            y_case_pred = torch.argmax(y_case_pred, dim=-1).cpu().numpy()
-            for ii in range(y_char_target.shape[0]):
-                lemma_target = batch[ii][3]
-                lemma_pred = ''
-                for jj in range(y_char_pred.shape[1]):
-                    if y_char_pred[ii, jj] == 0:
-                        break
-                    else:
-                        char = lemmatizer._char_list[y_char_pred[ii, jj].item()]
-                        if y_case_pred[ii, jj] == 1:
-                            char = char.upper()
-                        lemma_pred += char
-                if lemma_target == lemma_pred:
-                    good[ii] = 1
-                else:
-                    good[ii] = 0
+        # single value for early stopping
+        self._epoch_results = self._compute_early_stop(res)
+        self.log('val/early_meta', self._early_stop_meta_val)
 
-            total += y_char_target.shape[0]
-            ok += np.sum(good)
-            sys.stderr.flush()
+    def _compute_early_stop(self, res):
+        for lang in res:
+            if res[lang]["acc"] > self._res[lang]["acc"]:
+                self._early_stop_meta_val += 1
+                self._res[lang]["acc"] = res[lang]["acc"]
+                res[lang]["acc_best"] = True
+        return res
 
-    return ok / total
+    class PrintAndSaveCallback(pl.callbacks.Callback):
+        def __init__(self, store_prefix):
+            super().__init__()
+            self.store_prefix = store_prefix
 
+        def on_epoch_end(self, trainer, pl_module):
+            metrics = trainer.callback_metrics
+            epoch = trainer.current_epoch
 
-def _extract_data(dataset, unique=True):
-    data = []
-    visited = {}
-    for example in dataset.sequences:
-        seq = example[0]
-        lang_id = example[1]
-        for entry in seq:
-            key = '{0}_{1}_{2}'.format(lang_id, entry.upos, entry.word)
-            if not unique or key not in visited:
-                visited[key] = 1
-                if len(entry.word) < 100 and len(entry.lemma) < 100:
-                    data.append([entry.word, entry.upos, lang_id, entry.lemma])
-                else:
-                    print(entry.word, lang_id)
+            for lang in pl_module._epoch_results:
+                res = pl_module._epoch_results[lang]
+                if "acc_best" in res:
+                    trainer.save_checkpoint(self.store_prefix + "." + lang + ".best")
 
-    return data
+            trainer.save_checkpoint(self.store_prefix + ".last")
 
-
-def _get_batches(dataset, batch_size):
-    batches = []
-    batch = []
-    for ii in range(len(dataset)):
-        batch.append(dataset[ii])
-        if len(batch) == batch_size:
-            batches.append(batch)
-            batch = []
-    if len(batch) != 0:
-        batches.append(batch)
-    return batches
-
-
-def _get_case(char):
-    numbers = {'{0}'.format(ii): 1 for ii in range(10)}
-    if char in numbers:
-        return 1
-    elif char.lower() == char.upper():
-        return 2
-    elif char.lower() != char:
-        return 3
-    else:
-        return 4
-
-
-def _get_targets(batch, encodings):
-    lemmas = [example[3] for example in batch]
-    max_len = max([len(lemma) for lemma in lemmas]) + 1
-    targets_char = np.zeros((len(lemmas), max_len))
-    targets_case = np.zeros((len(lemmas), max_len))
-    for ii in range(targets_char.shape[0]):
-        for jj in range(targets_char.shape[1]):
-            if jj < len(lemmas[ii]):
-                char = lemmas[ii][jj]
-                if char.lower() != char:
-                    case = 1
-                else:
-                    case = 2
-                targets_case[ii, jj] = case
-                if char.lower() in encodings.char2int:
-                    targets_char[ii, jj] = encodings.char2int[char.lower()]
-                else:
-                    targets_char[ii, jj] = encodings.char2int['<UNK>']
-
-    return targets_char, targets_case
-
-
-def _start_train(lemmatizer, trainset, devset, params):
-    import random
-    patience_left = params.patience
-    trainer = torch.optim.Adam(lemmatizer.parameters(), lr=1e-4)
-    critetion = torch.nn.CrossEntropyLoss()
-    # prepare dataset
-    trainset = _extract_data(trainset, unique=True)
-    devset = _extract_data(devset, unique=True)
-    epoch = 0
-    best_score = 0  # _eval(lemmatizer, devset)
-
-    lemmatizer._config.save('{0}.conf'.format(params.store))
-    lemmatizer._encodings.save('{0}.encodings'.format(params.store))
-    sys.stdout.write('Devset accuracy {0}\n'.format(best_score))
-    sys.stdout.flush()
-
-    while patience_left > 0:
-        patience_left -= 1
-        epoch += 1
-        sys.stdout.write('Epoch {0}\n'.format(epoch))
-        sys.stdout.flush()
-        sys.stdout.write('\tShuffling training data\n')
-        sys.stdout.flush()
-        random.shuffle(trainset)
-        batches = _get_batches(trainset, params.batch_size)
-        pgb = tqdm.tqdm(batches, desc='\tloss=NaN', ncols=80)
-        total_loss = 0
-        lemmatizer.train()
-        for batch in pgb:
-            x_chars = []
-            x_lang = []
-            x_upos = []
-            x_lemmas = []
-            for ii in range(len(batch)):
-                x_chars.append(batch[ii][0])
-                x_lang.append(batch[ii][1])
-                x_upos.append(batch[ii][2])
-                x_lemmas.append(batch[ii][3])
-
-            y_char_target, y_case_target = _get_targets(batch, encodings=lemmatizer._encodings)
-            y_char_pred, y_case_pred = lemmatizer(x_chars, x_upos, x_lang, gs_output=x_lemmas)
-            y_char_target = torch.tensor(y_char_target, dtype=torch.long, device=lemmatizer._get_device()).view(-1)
-            y_case_target = torch.tensor(y_case_target, dtype=torch.long, device=lemmatizer._get_device()).view(-1)
-            y_char_pred = y_char_pred.view(-1, y_char_pred.shape[-1])
-            y_case_pred = y_case_pred.view(-1, y_case_pred.shape[-1])
-            loss = critetion(y_char_pred, y_char_target) + critetion(y_case_pred, y_case_target)
-            trainer.zero_grad()
-            loss.backward()
-            trainer.step()
-            loss = loss.item()
-            total_loss += loss
-            pgb.set_description('\tloss={0:.4f}'.format(loss))
-            sys.stderr.flush()
-
-        acc = _eval(lemmatizer, devset)
-        sys.stderr.flush()
-        sys.stdout.write('\t\tAVG epoch loss {0}\n'.format(total_loss / len(batches)))
-        sys.stdout.write('\t\tDevset accuracy {0}\n'.format(acc))
-        sys.stdout.flush()
-        if acc > best_score:
-            best_score = acc
-            fname = '{0}.best'.format(params.store)
-            sys.stdout.write('\t\tStoring {0}\n'.format(fname))
-            lemmatizer.save(fname)
-            patience_left = params.patience
-
-        fname = '{0}.last'.format(params.store)
-        sys.stdout.write('\t\tStoring {0}\n'.format(fname))
-        lemmatizer.save(fname)
-
-
-def do_train(params):
-    import json
-    ds_list = json.load(open(params.train_file))
-    train_list = []
-    dev_list = []
-    for ii in range(len(ds_list)):
-        train_list.append(ds_list[ii][1])
-        dev_list.append(ds_list[ii][2])
-
-    from cube.io_utils.conll import Dataset
-    from cube.io_utils.encodings import Encodings
-
-    trainset = Dataset()
-    devset = Dataset()
-    for ii, train, dev in zip(range(len(train_list)), train_list, dev_list):
-        trainset.load_language(train, ii, ignore_compound=True)
-        devset.load_language(dev, ii, ignore_compound=True)
-    encodings = Encodings()
-    if params.resume:
-        encodings.load('{0}.encodings'.format(params.store))
-    else:
-        encodings.compute(trainset, devset, word_cutoff=2)
-
-    config = LemmatizerConfig(filename=params.config_file)
-    config.num_languages = len(train_list)
-    lemmatizer = Lemmatizer(config, encodings, num_languages=len(train_list))
-    if params.resume:
-        lemmatizer.load('{0}.last'.format(params.store))
-    lemmatizer.to(params.device)
-    _start_train(lemmatizer, trainset, devset, params)
-
-
-def do_test():
-    pass
-
-
-if __name__ == '__main__':
-    parser = optparse.OptionParser()
-    parser.add_option('--train', action='store', dest='train_file',
-                      help='Start building a tagger model')
-    parser.add_option('--config', action='store', dest='config_file',
-                      help='Use this configuration file for tagger')
-    parser.add_option('--patience', action='store', type='int', default=20, dest='patience',
-                      help='Number of epochs before early stopping (default=20)')
-    parser.add_option('--store', action='store', dest='store', help='Output base', default='tagger')
-    parser.add_option('--batch-size', action='store', type='int', default=32, dest='batch_size',
-                      help='Number of epochs before early stopping (default=32)')
-    parser.add_option('--device', action='store', dest='device', default='cpu',
-                      help='What device to use for models: cpu, cuda:0, cuda:1 ...')
-    parser.add_option('--test', action='store_true', dest='test', help='Test the traine model')
-    parser.add_option('--test-file', action='store', dest='test_file')
-    parser.add_option('--lang-id', action='store', dest='lang_id', type='int', default=0)
-    parser.add_option('--model-base', action='store', dest='model_base')
-    parser.add_option('--resume', action='store_true', dest='resume')
-
-    (params, _) = parser.parse_args(sys.argv)
-
-    if params.train_file:
-        do_train(params)
-    elif params.test:
-        do_test(params)
+            s = "{0:30s}\tACC".format("Language")
+            print("\n\n\t" + s)
+            print("\t" + ("=" * (len(s) + 16)))
+            for lang in pl_module._language_codes:
+                acc = metrics["val/ACC/{0}".format(lang)]
+                msg = "\t{0:30s}:\t{1:.4f}".format(lang, acc)
+                print(msg)
+            print("\n")
