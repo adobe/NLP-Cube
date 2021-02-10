fec6a2dfa3002c0e27a61acd7a0e7e81b26e3449
Tiberiu Boros <tiberiu44@gmail.com>
2021-02-10 07:37:42 +0200

    Mixed test fasttext+transformer

--- test.py
+++ test.py
@@ -1,52 +1,76 @@
 import sys
+
 sys.path.append('')
 import torch
 from cube3.networks.tokenizer import Tokenizer
 from cube3.networks.tagger import Tagger
 from cube3.io_utils.config import TokenizerConfig, TaggerConfig, ParserConfig
 from cube3.io_utils.encodings import Encodings
-from cube3.networks.utils_tokenizer import TokenCollateFTLanguasito
-from cube3.networks.lm import LMHelperFT
+from cube3.networks.utils_tokenizer import TokenCollateFTLanguasito, TokenCollateHF
+from cube3.networks.lm import LMHelperFT, LMHelperHF
 from cube3.networks.utils import MorphoCollate
 from cube3.networks.parser import Parser
 
+# # tokenizer
+# enc = Encodings()
+# enc.load('data/tokenizer-ro-transformer.encodings')
+#
+# config = TokenizerConfig()
+# tokenizer = Tokenizer(config, enc, language_codes=['ro_nonstandard', 'ro_rrt'], ext_word_emb=[768 for _ in range(13)])
+#
+# model = torch.load('data/tokenizer-ro-transformer.ro_rrt.sent', map_location='cpu')
+#
+# tokenizer.load_state_dict(model['state_dict'])
+#
+# collate = TokenCollateHF(enc, lm_model='xlm-roberta-base', lm_device='cpu')
+# text = open('corpus/ud-treebanks-v2.5/UD_Romanian-RRT/ro_rrt-ud-test.txt').read()
+# # text = 'Și eu am mere. Ana are mere, dar nu are pere. Acesta este un test.'
+# d = tokenizer.process(text, collate, lang_id=1, batch_size=4)
 # tokenizer
 enc = Encodings()
 enc.load('data/tokenizer-ro-fasttext.encodings')
 
 config = TokenizerConfig()
-tokenizer = Tokenizer(config, enc, language_codes=['ro_nonstandard', 'ro_rrt'], ext_word_emb=300)
+tokenizer = Tokenizer(config, enc, language_codes=['ro_rrt'], ext_word_emb=[300])
 
 model = torch.load('data/tokenizer-ro-fasttext.ro_rrt.sent', map_location='cpu')
 
 tokenizer.load_state_dict(model['state_dict'])
 
-collate = TokenCollateFTLanguasito(enc, lm_model='fasttext:ro')
+collate = TokenCollateFTLanguasito(enc, lm_model='fasttext:ro', lm_device='cpu')
 text = open('corpus/ud-treebanks-v2.5/UD_Romanian-RRT/ro_rrt-ud-test.txt').read()
-d = tokenizer.process(text, collate, lang_id=1, batch_size=4)
+# text = 'Și eu am mere. Ana are mere, dar nu are pere. Acesta este un test.'
+d = tokenizer.process(text, collate, lang_id=0, batch_size=4)
+for ii in range(len(d.sentences)):
+    d.sentences[ii].lang_id = 1
 
-helper = LMHelperFT(model='ro')
-helper.apply(d)
+# helper = LMHelperFT(model='ro')
+# helper.apply(d)
 
-# tagger
-enc = Encodings()
-enc.load('data/tagger-ro-fasttext.encodings')
-model = torch.load('data/tagger-ro-fasttext.ro_rrt.upos', map_location='cpu')
-config = TaggerConfig()
-config.load('data/tagger-ro-fasttext.config')
-tagger = Tagger(config, enc, ext_word_emb=helper.get_embedding_size(), language_codes=['ro_nonstandard', 'ro_rrt'])
-tagger.load_state_dict(model['state_dict'])
-collate = MorphoCollate(enc)
-d = tagger.process(d, collate)
+# # tagger
+# enc = Encodings()
+# enc.load('data/tagger-ro-fasttext.encodings')
+# model = torch.load('data/tagger-ro-fasttext.ro_rrt.upos', map_location='cpu')
+# config = TaggerConfig()
+# config.load('data/tagger-ro-fasttext.config')
+# tagger = Tagger(config, enc, ext_word_emb=helper.get_embedding_size(), language_codes=['ro_nonstandard', 'ro_rrt'])
+# tagger.load_state_dict(model['state_dict'])
+# collate = MorphoCollate(enc)
+# d = tagger.process(d, collate)
 
 # parser
+# del helper
+helper = LMHelperHF(model='xlm-roberta-base')
+helper.apply(d)
 enc = Encodings()
-enc.load('data/parser-ro-fasttext.encodings')
-model = torch.load('data/parser-ro-fasttext.ro_rrt.uas', map_location='cpu')
+enc.load('data/parser-ro-transformer.encodings')
+collate = MorphoCollate(enc)
+model = torch.load('data/parser-ro-transformer.ro_rrt.uas', map_location='cpu')
 config = ParserConfig()
-config.load('data/parser-ro-fasttext.config')
-tagger = Parser(config, enc, ext_word_emb=helper.get_embedding_size(), language_codes=['ro_nonstandard', 'ro_rrt'])
-tagger.load_state_dict(model['state_dict'])
-d = tagger.process(d, collate)
+config.load('data/parser-ro-transformer.config')
+parser = Parser(config, enc, ext_word_emb=helper.get_embedding_size(), language_codes=['ro_nonstandard', 'ro_rrt'])
+parser.load_state_dict(model['state_dict'])
+d = parser.process(d, collate)
 
 print(d)
+print("")
